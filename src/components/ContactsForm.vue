05e6dea8731ef9746db77ccb155fd005f1188c35
John Fitzgerald <johnf@datacom.co.nz>
2019-12-18 15:52:38 +1300

    Add bootstrap

--- src/components/ContactsForm.vue
+++ src/components/ContactsForm.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        <p>test</p>
+        <h2>Input your contact details below</h2>
+        <form action="">
+            <label for="firstName">First name</label>
+            <input type="text" id="firstName" placeholder="First name" class="form-control">
+
+            <label for="lastName">Last name</label>
+            <input type="text" id="lastName" placeholder="Last name" class="form-control">
+
+            <label for="emailAddress">Email address</label>
+            <input type="email" id="emailAddress" placeholder="Enter email" class="form-control" aria-describedby="emailHelp">
+            
+            <input type="submit" id="submit" value="Submit" class="btn btn-primary">
+        </form>
     </div>
 </template>
 
