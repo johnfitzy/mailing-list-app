3d2c99fd91ae52e128ecf1dc3dfaa57bba14bad1
John Fitzgerald <johnf@datacom.co.nz>
2019-12-18 15:23:51 +1300

    Add test form component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-
+    <ContactsForm/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import ContactsForm from '@/components/ContactsForm'
 
 export default {
   name: 'home',
   components: {
-
+    ContactsForm
   }
 }
 </script>
